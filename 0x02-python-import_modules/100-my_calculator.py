ca8f4a07ec92d1ddcc4b819e246f325149b080e8
Faith Njambi <njambifaith30@gmail.com>
2023-08-12 12:36:55 +0300

    :bug: Fixing a code error and pycodestyle requirement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -28,4 +28,4 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(a, b, operator, result))
+    print("{} {} {} = {}".format(a, operator, b, result))
