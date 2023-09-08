9400d6c654204fa8ea79e5b657c29fcd93742a45
Faith Njambi <njambifaith30@gmail.com>
2023-09-08 19:08:11 +0300

    Task 0: Write a function that adds 2 integers.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Defines a function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    """
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        the sum of the two integers.
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return (a + b)
